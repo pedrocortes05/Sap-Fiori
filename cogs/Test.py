95f9d5c1017cfcd4617d082353cdc7415f821111
pedrocortes05 <pedrocortespro@gmail.com>
2022-03-01 10:18:54 -0600

    Bot running

--- cogs/Test.py
+++ cogs/Test.py
@@ -1,4 +1,3 @@
-from http import client
 import discord
 import asyncio
 import traceback
@@ -7,68 +6,48 @@ from discord.ext import commands
 from timeit import default_timer as timer
 
 import main
-import functions
-importlib.reload(functions)
+#import functions
+#importlib.reload(functions)
 
 class Test(commands.Cog):
 
-	def __init__(self, client):
-		self.client = client
+    def __init__(self, client):
+        self.client = client
 
-	@commands.group(invoke_without_command=True)
-	async def help(self, ctx):
-		embed = discord.Embed(title="Mapgame Commands", color=ctx.me.color)
-		embed.set_author(name="Mapgame", icon_url="https://cdn.discordapp.com/avatars/908858108246384721/9f90605cce51952bd639bd88ac807bff.webp?size=128")
-		prefix = main.get_prefix(main.client, ctx)
-
-		player_commands = f"""**{prefix}Stockpile**: - Inspect a nations stockpile
-								**{prefix}Remind**: - Set a reminder for the next X turns"""
-
-		mapgame_commands = f"""**{prefix}BuildCity**: - Constructing buildings in your city
-								**{prefix}RuralTax**: - Change the taxes on your rural population"""
-
-		admin_commands = f"""**{prefix}NewTurn**
-							**{prefix}SumMoney**"""
-
-		embed.add_field(name="__Player__", value=player_commands, inline=False)
-		embed.add_field(name="__Mapgame__", value=mapgame_commands, inline=False)
-		embed.add_field(name="__Moderation__", value=admin_commands, inline=False)
-		await ctx.send(embed=embed)
-
-	@commands.commnad()
+    @commands.command()
     async def Username(self, ctx, username):
         print(username)
 
-    @commands.commnad()
+    @commands.command()
     async def Password(self, ctx, password):
         print(password)
 
 
-	#Error handling
-	@commands.Cog.listener()
-	async def on_command_error(self, ctx, error):
-		if isinstance(error, commands.CommandNotFound):
-			await ctx.send("Invalid command")
+    #Error handling
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("Invalid command")
 
-	@Username.error
-	async def Username_error(self, ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			await ctx.send(f"Syntax: **{main.get_prefix(main.client, ctx)}Username <Username>**")
-		else:
-			await ctx.send(f"**Error ocurred**")
-			print(traceback.format_exc())
-		
-		print(error)
+    @Username.error
+    async def Username_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"Syntax: **{main.get_prefix(main.client, ctx)}Username <Username>**")
+        else:
+            await ctx.send(f"**Error ocurred**")
+            print(traceback.format_exc())
+        
+        print(error)
 
     @Password.error
-	async def Password_error(self, ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			await ctx.send(f"Syntax: **{main.get_prefix(main.client, ctx)}Password <Password>**")
-		else:
-			await ctx.send(f"**Error ocurred**")
-			print(traceback.format_exc())
-		
-		print(error)
+    async def Password_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"Syntax: **{main.get_prefix(main.client, ctx)}Password <Password>**")
+        else:
+            await ctx.send(f"**Error ocurred**")
+            print(traceback.format_exc())
+        
+        print(error)
 
 
 def setup(client):
--- main.py
+++ main.py
@@ -3,14 +3,26 @@ import discord
 import asyncio
 from discord.ext import commands, tasks
 
+def get_prefix(client, message):
+	with open("prefixes.json", 'r') as f:
+		pass
+		#prefixes = json.load(f)
+
+	try:
+		prefix = prefixes[str(message.guild.id)]
+	except:
+		prefix = '#'
+
+	return prefix
+
 
 token = "OTQ4MjE2Njk0NzY0MTA5ODk3.Yh4lnQ.kZEqV_UDBM6fsmIrh-bVdTLd6p0"
-client = commands.Bot(case_insensitive=True)
-client.remove_command("help")
+client = commands.Bot(command_prefix = "#", case_insensitive=True)
+#client.remove_command("help")
 
 @client.event
 async def on_ready():
-	await client.change_presence(status=discord.Status.online, activity=discord.Game("Google Sheets"))
+	await client.change_presence(status=discord.Status.online, activity=discord.Game("Generating QR codes"))
 	print(f"Logged in as {client.user}")
 
 @client.command()
