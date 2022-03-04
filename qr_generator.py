8d93a329521342a2b765204b5d1ce56f07c97358
pedrocortes05 <pedrocortespro@gmail.com>
2022-03-03 19:45:52 -0600

    idk

--- qr_generator.py
+++ qr_generator.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image
 
 import main
 
